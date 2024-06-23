4e6b6718d9ae0e857bd270dbf9590502220d8aad
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-23 20:49:50 +0100

    feat(main.ts): wrap dispatcher function with helper.friendlyCall
    The dispatcher function 'open' in main.ts has been wrapped with
    helper.friendlyCall. This change ensures that any errors thrown within
    the function are handled gracefully and presented in a user-friendly
    manner. The function's logic remains unchanged.

--- denops/ray-so/main.ts
+++ denops/ray-so/main.ts
@@ -1,5 +1,6 @@
 import { Denops } from "https://deno.land/x/denops_std@v6.5.0/mod.ts";
 import { g } from "https://deno.land/x/denops_std@v6.5.0/variable/variable.ts";
+import * as helper from "https://deno.land/x/denops_std@v6.5.0/helper/mod.ts";
 
 import { systemopen } from "jsr:@lambdalisue/systemopen@1.0.0";
 import * as U from "jsr:@core/unknownutil@3.18.1";
@@ -11,41 +12,44 @@ const isRange = U.isTupleOf([U.isNumber, U.isNumber] as const);
 
 export function main(denops: Denops) {
   denops.dispatcher = {
-    async open(args, range) {
-      U.assert(args, U.isArrayOf(U.isString), { name: "args" });
-      U.assert(range, U.isUnionOf([U.isUndefined, isRange]), { name: "range" });
+    open: (args, range) =>
+      helper.friendlyCall(denops, async () => {
+        U.assert(args, U.isArrayOf(U.isString), { name: "args" });
+        U.assert(range, U.isUnionOf([U.isUndefined, isRange]), {
+          name: "range",
+        });
 
-      /** get selected text. if range is undefined, get all text */
-      const codeArray = range === undefined
-        ? await denops.call("getline", 1, "$")
-        : await denops.call("getline", range[0], range[1]);
-      U.assert(codeArray, U.isArrayOf(U.isString));
-      const code = codeArray.join("\n");
+        /** get selected text. if range is undefined, get all text */
+        const codeArray = range === undefined
+          ? await denops.call("getline", 1, "$")
+          : await denops.call("getline", range[0], range[1]);
+        U.assert(codeArray, U.isArrayOf(U.isString));
+        const code = codeArray.join("\n");
 
-      const path = args.at(0) ?? await denops.eval("expand('%:t')");
+        const path = args.at(0) ?? await denops.eval("expand('%:t')");
 
-      const theme = await g.get(denops, "ray_so_theme");
-      const padding = await g.get(denops, "ray_so_padding");
-      const background = await g.get(denops, "ray_so_background");
-      const darkmode = await g.get(denops, "ray_so_darkmode");
+        const theme = await g.get(denops, "ray_so_theme");
+        const padding = await g.get(denops, "ray_so_padding");
+        const background = await g.get(denops, "ray_so_background");
+        const darkmode = await g.get(denops, "ray_so_darkmode");
 
-      const filetype = await denops.eval("&filetype");
+        const filetype = await denops.eval("&filetype");
 
-      const options = U.ensure(
-        {
-          code,
-          title: path,
-          theme,
-          padding,
-          background,
-          darkmode,
-          language: filetype,
-        } as const,
-        isOptions,
-      );
+        const options = U.ensure(
+          {
+            code,
+            title: path,
+            theme,
+            padding,
+            background,
+            darkmode,
+            language: filetype,
+          } as const,
+          isOptions,
+        );
 
-      const url = getRaySoUrl({ ...options, code });
-      await systemopen(url);
-    },
+        const url = getRaySoUrl({ ...options, code });
+        await systemopen(url);
+      }),
   };
 }
